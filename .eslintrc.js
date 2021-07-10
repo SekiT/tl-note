8cfec65cfe664b0dc1b4903846f7b32ff509c4c5
Takaaki Seki <ormscrongers@gmail.com>
2021-07-11 00:02:34 +0900

    Setup bundle and lint environment

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: 'eslint-config-airbnb-base',
+  env: {
+    browser: true,
+  },
+  settings: {
+    // Resolve aliased imports
+    'import/resolver': {
+      alias: [
+        ['@', './'],
+      ],
+    },
+  },
+  rules: {
+    // We use devDependencies in rollup-config, which is not included in eslint-config-airbnb-base
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: ['rollup-config.js'],
+      optionalDependencies: false,
+    }],
+  },
+};
---
+++ rollup-config.js
@@ -0,0 +1,38 @@
+import alias from '@rollup/plugin-alias';
+import resolve from '@rollup/plugin-node-resolve';
+import esbuild from 'rollup-plugin-esbuild';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+const unbreakTemplatePlugin = {
+  name: 'unbreak-template',
+  transform(code) {
+    return {
+      code: code.replace(/`[^`]*`/g, (template) => template.replace(/\n/g, '')),
+    };
+  },
+};
+
+const commonPlugins = [
+  resolve(),
+  alias({ entries: { '@': process.cwd() } }),
+];
+
+const envDependentPlugins = process.env.PRODUCTION ? [
+  esbuild({ minify: true }),
+  unbreakTemplatePlugin,
+] : [
+  serve({ contentBase: 'build', open: true }),
+  livereload(),
+];
+
+export default {
+  input: 'index.js',
+  plugins: [...commonPlugins, ...envDependentPlugins],
+  context: 'null',
+  moduleContext: 'null',
+  output: {
+    file: 'build/bundle.js',
+    format: 'iife',
+  },
+};
