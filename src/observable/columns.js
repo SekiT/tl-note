ba4ac30c993a16165ce5693f7803cd23505fb8cf
Takaaki Seki <ormscrongers@gmail.com>
2021-07-11 22:04:32 +0900

    Width of columns

--- src/observable/columns.js
+++ src/observable/columns.js
@@ -1,9 +1,24 @@
 import { observable } from 'sinuous/observable';
 
 const columns = observable([
-  { id: 'c1', name: 'A氏', color: [0, 0, 128] },
-  { id: 'c2', name: 'B君', color: [255, 128, 128] },
-  { id: 'c3', name: 'C様', color: [128, 255, 128] },
+  {
+    id: 'c1',
+    name: 'A氏',
+    color: [0, 0, 128],
+    width: 33,
+  },
+  {
+    id: 'c2',
+    name: 'B君',
+    color: [255, 128, 128],
+    width: 33,
+  },
+  {
+    id: 'c3',
+    name: 'C様',
+    color: [128, 255, 128],
+    width: 33,
+  },
 ]);
 
 export default columns;
--- src/view/columns.js
+++ src/view/columns.js
@@ -2,25 +2,31 @@ import { html } from 'sinuous';
 import { map } from 'sinuous/map';
 import columns from '@/observable/columns';
 
-const columnStyle = (color) => ({
+const columnStyle = ({ color, width }) => ({
   display: 'inline',
+  width: `${width}%`,
   'text-align': 'center',
   'background-color': `rgb(${color.join(',')})`,
   color: color[0] + color[1] + color[2] < 384 ? 'white' : 'black',
 });
 
-const columnView = ({ name, color }) => html`
-  <div style=${columnStyle(color)}>${name}</div>
+const columnView = (column) => html`
+  <div style=${columnStyle(column)}>${column.name}</div>
 `;
 
 const containerStyle = {
-  width: '90%',
-  margin: '0 5%',
+  display: 'flex',
+  width: '80%',
+  margin: '0 5% 0 15%',
+};
+
+const buttonStyle = {
+  width: '5%',
 };
 
 export default () => html`
   <div style=${containerStyle}>
     ${map(columns, columnView)}
-    <button>+</button>
+    <button style=${buttonStyle}>+</button>
   </div>
 `;
