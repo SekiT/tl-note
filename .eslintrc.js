5b179c288777adff143778893334abcf8c36a10b
Takaaki Seki <ormscrongers@gmail.com>
2021-07-13 22:52:12 +0900

    Disable prefer-default-export

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     },
   },
   rules: {
+    // It often decrease exntensibility
+    'import/prefer-default-export': 'off',
     // We use devDependencies in rollup-config, which is not included in eslint-config-airbnb-base
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: ['rollup-config.js'],
